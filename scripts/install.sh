c4cc248763d46d9c47bf7d3fb338a3a1318e87ea
xanhacks <xanhacks@protonmail.com>
2023-02-11 19:59:59 +0100

    add notify

--- scripts/install.sh
+++ scripts/install.sh
@@ -122,6 +122,21 @@ install_katana() {
     log "${tool} installed!"
 }
 
+install_notify() {
+	local tool='notify'
+	local tmp_dir="$(mktemp -d)"
+
+    log "Installing ${tool}..."
+	wget -q "https://github.com/projectdiscovery/${tool}/releases/download/v${NOTIFY_VERSION}/${tool}_${NOTIFY_VERSION}_linux_amd64.zip" \
+        -O "${tmp_dir}/${tool}_linux_amd64.zip"
+
+    (cd "${tmp_dir}" && unzip -q "${tmp_dir}/${tool}_linux_amd64.zip")
+    mv "${tmp_dir}/${tool}" "${BIN_DIR}/${tool}"
+    rm -rf "${tmp_dir}"
+    log "${tool} installed!"
+}
+
+
 
 mkdir "$BIN_DIR"
 
@@ -134,3 +149,4 @@ install_drupwn
 install_subfinder
 install_asnmap
 install_katana
+install_notify
